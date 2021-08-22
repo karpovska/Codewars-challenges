c33fe224431ba458d734312646256ee52ddaa808
karpovska <oksanachv@gmail.com>
2021-08-22 11:28:14 +0200

    arrowfunction solution added

--- Categorize_new_member.js
+++ Categorize_new_member.js
@@ -1,14 +1,8 @@
-testArr = [[18, 20],[45, 2],[61, 12],[37, 6],[21, 21],[78, 9]];
+testArr = [[18, 20],[65, 10],[61, 12],[37, 6],[21, 21],[78, 9]];
 
 function openOrSenior(data){
-    let isSenior = data.map(m => {
-      if(m[0] >= 55 && m[1] > 7)
-        return "Senior"
-      else 
-        return "Open"
-    })
+    let isSenior = data.map(m => (m[0] >= 55 && m[1] > 7) ? "Senior" : " Open");
     console.log(isSenior);
   }
-
 openOrSenior(testArr);
 
