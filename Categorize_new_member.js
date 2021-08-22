f0c5c00f7680e1ef9ad64815b7dbfa2c3a1a95bf
karpovska <oksanachv@gmail.com>
2021-08-22 11:24:17 +0200

    bug fixed

--- Categorize_new_member.js
+++ Categorize_new_member.js
@@ -2,7 +2,7 @@ testArr = [[18, 20],[45, 2],[61, 12],[37, 6],[21, 21],[78, 9]];
 
 function openOrSenior(data){
     let isSenior = data.map(m => {
-      if(m[0] >= 55 && m[1] >= 7)
+      if(m[0] >= 55 && m[1] > 7)
         return "Senior"
       else 
         return "Open"
